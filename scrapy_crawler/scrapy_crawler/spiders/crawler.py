788e4163c5e12066d8a1e106a797c8bb4537eb16
Nahuel <m4il_cuent4s@outlook.com.ar>
2015-11-08 19:30:33 -0300

    agrego lo modificado por el gigo gonza

--- scrapy_crawler/scrapy_crawler/spiders/crawler.py
+++ scrapy_crawler/scrapy_crawler/spiders/crawler.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy_crawler.items import AutorItem, PublicacionItem
@@ -22,9 +23,9 @@ class crawler(scrapy.Spider):
 			yield scrapy.Request(url, callback=self.parse_web1)
 
 	def parse_web1(self, response):
-		for sel in response.xpath("//div[@id='sites-canvas-main-content']/div[@class='sites-layout-name-right-sidebar-hf sites-layout-vbox']"):
+		'''for sel in response.xpath("//div[@id='sites-canvas-main-content']/div[@class='sites-layout-name-right-sidebar-hf sites-layout-vbox']"):
 			print (sel.xpath("//div[@id='sites-canvas-main-content']/div[@class='sites-layout-name-right-sidebar-hf sites-layout-vbox']/div/div/div") )
-			print("HEY HEY")
+			print("HEY HEY")'''
 		'''for sel in response.xpath("//div[@id='ds-main']/div[@id='ds-content-wrapper']/div[@id='ds-content']"):
 			publicacion = PublicacionItem()
 			print (sel.xpath("//div[@id='ds-main']/div[@id='ds-content-wrapper']/div[@id='ds-content']/div[@id='ds-body']/div[@id='aspect_artifactbrowser_ItemViewer_div_item-view']/div[@class='item-summary-view-metadata']/h1/text()").extract()[0].strip())#titulo
@@ -34,7 +35,7 @@ class crawler(scrapy.Spider):
 			print( response.xpath("//meta[@scheme='DCTERMS.URI']/@content").extract()[0])#url base
 			print (sel.xpath("//div[@id='ds-main']/div[@id='ds-content-wrapper']/div[@id='ds-content']/div[@id='ds-body']/div[@id='aspect_artifactbrowser_ItemViewer_div_item-view']/div/div/div/a[@class='image-link']/@href").extract()[0].strip())#link'''
 
-		'''i=0
+		i=0
 		for sel in response.xpath("//ul[@class='d3s-revista']"):
 			publicaciones = sel.xpath("//li[contains(p, 'Scientific Letters')]/p[@class='d3s-titulo-post']/text()").extract()
 			autores = sel.xpath("//li[contains(p, 'Scientific Letters')]/p[@class='d3s-titulo-autores']/text()").extract()
@@ -50,7 +51,7 @@ class crawler(scrapy.Spider):
 					publicacion['url_link'] = links[o]
 					yield publicacion
 					o+=1
-				i+=1'''
+				i+=1
 
 		'''for sel in response.xpath("//div[@id='ds-main']/div[@id='ds-content-wrapper']/div[@id='ds-content']"):
 			publicacion = PublicacionItem()
