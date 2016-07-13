ca677869aa3d42f7b9c72009fa17d6de498555fe
nahuel <nahuel@debian>
2016-07-12 21:17:47 -0300

    script arreglado

--- start_webbot.sh
+++ start_webbot.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 SCRIPTPATH=$(pwd)
-cd ${SCRIPTPATH}/scrapy_crawler
+cd ${SCRIPTPATH}/proyecto_soft/scrapy_crawler
 
 PATH=$PATH:/usr/local/bin
 export PATH
 
 scrapy crawl crawler
 
-wget http://localhost:8983/solr/Index/dataimport?command=full-import
+wait
+
+#wget http://localhost:8983/solr/Index/dataimport?command=full-import
