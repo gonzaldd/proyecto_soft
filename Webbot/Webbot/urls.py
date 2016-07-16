cea4e2eeefaeeb4d1c2cef8ddcd4e2b7a06aed66
nahuel <nahuel@debian>
2016-07-15 22:39:59 -0300

    Mas modificaciones por cambio de nombre de bd

--- Webbot/Webbot/urls.py
+++ Webbot/Webbot/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
-from WB_app.views import *
+from ps_wdb2015.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
