6395ce30866ff2e2e6cffe4782cbf3f6b5fbfdbe
nahuel <nahuel@debian>
2016-07-16 12:45:05 -0300

    modificado el webbot, el script todavia no anda

--- start_webbot.sh
+++ start_webbot.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-SCRIPTPATH=$(pwd)
-cd ${SCRIPTPATH}/proyecto_soft/scrapy_crawler
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+cd ${SCRIPTPATH}/scrapy_crawler
 
 PATH=$PATH:/usr/local/bin
 export PATH
 
 wget 'http://localhost:8983/solr/Index/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
+
 
 scrapy crawl crawler
 
